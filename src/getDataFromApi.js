710b17b7738d333d2c711698befdb7b1e790b664
kapi104 <skorupa7979@gmail.com>
2023-10-04 15:58:52 +0200

    added function for getting data from api

---
+++ src/getDataFromApi.js
@@ -0,0 +1,21 @@
+// retrieves data for inputted city from WeatherApi
+
+const getApiData = async (city) => {
+  try {
+    let data = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=e3ed4efcf7394dceab5183635231909&q=${city}&aqi=no`,
+      { mode: 'cors' },
+    );
+
+    data = await data.json();
+
+    console.log(data);
+
+    return data;
+    
+  } catch (error) {
+    console.log('connection error');
+  }
+};
+
+export default getApiData;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import moment from 'moment';
+
+import getApiData from './getDataFromApi';
+
+getApiData('london');
