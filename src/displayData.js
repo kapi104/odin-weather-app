c92824b4ca4fbce2a506985d6e79a1d8a1bdb5ee
kapi104 <skorupa7979@gmail.com>
2023-10-05 19:13:48 +0200

    added function displaying current weather

--- src/displayData.js
+++ src/displayData.js
@@ -1,4 +1,14 @@
-const displayCurrentWeather = (weather) => {};
+const displayCurrentWeather = (weather) => {
+  document.querySelector('.city').innerHTML = weather.location
+  document.querySelector('.localTime').innerHTML = weather.localHour
+  document.querySelector('.currentCondition > img').src = weather.current.icon
+  document.querySelector('.currentTemp').innerHTML = `${weather.current.temp}°C`
+  
+  document.querySelector('.feelsLike > span').innerHTML = `${weather.current.feelsLike}°C`
+  document.querySelector('.pressure > span').innerHTML = `${weather.current.pressure} hPa`
+  document.querySelector('.windspeed > span').innerHTML = `${weather.current.windSpeed} km/h`
+  document.querySelector('.humidity > span').innerHTML = `${weather.current.humidity}%`
+};
 
 const displayData = (weather) => {
   displayCurrentWeather(weather);
