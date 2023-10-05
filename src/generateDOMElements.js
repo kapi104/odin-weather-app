d4bd12b9dd860573fb31c68b93b08134ae12baf6
kapi104 <skorupa7979@gmail.com>
2023-10-05 21:17:53 +0200

    added function displaying forecast for each hour

--- src/generateDOMElements.js
+++ src/generateDOMElements.js
@@ -1,4 +1,28 @@
-const generateDays = (forecastDay) => {
+const generateHours = (hourData, day) => {
+  const hour = document.createElement('div');
+  hour.classList.add('hour');
+
+  const time = document.createElement('div');
+  time.classList.add('time');
+  time.innerHTML = hourData.time;
+
+  const icon = document.createElement('div');
+  icon.classList.add('icon');
+  const img = document.createElement('img');
+  img.src = hourData.condition;
+
+  icon.appendChild(img);
+
+  const temp = document.createElement('div');
+  temp.classList.add('temp');
+  temp.innerHTML = `${hourData.temp}°C`;
+
+  hour.append(time, icon, temp);
+
+  day.appendChild(hour);
+};
+
+const generateDays = (dayData) => {
   const day = document.createElement('div');
   day.classList.add('day');
 
@@ -7,20 +31,25 @@ const generateDays = (forecastDay) => {
 
   const date = document.createElement('div');
   date.classList.add('date');
-  date.innerHTML = forecastDay.date;
+  date.innerHTML = dayData.date;
 
   const icon = document.createElement('div');
   icon.classList.add('icon');
   const img = document.createElement('img');
-  img.src = forecastDay.dayIcon;
+  img.src = dayData.dayIcon;
   icon.appendChild(img);
 
   const temp = document.createElement('div');
   temp.classList.add('temp');
-  temp.innerHTML = forecastDay.dayAvgTemp;
+  temp.innerHTML = `${dayData.dayAvgTemp}°C`;
 
   dayForecast.append(date, icon, temp);
+
   day.appendChild(dayForecast);
+
+  dayData.hour.forEach((h) => {
+    generateHours(h, day);
+  });
 
   document.querySelector('aside').appendChild(day);
 };
